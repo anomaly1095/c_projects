9d4bd29eb2f701a0f149de2e04d32d9eac0935cf
Somali99 <syoyo1095@gmail.com>
2024-03-09 16:07:55 +0100

    Writing sudoku

--- games/sudoku.c
+++ games/sudoku.c
@@ -8,11 +8,103 @@ int main(int argc, char **argv){
     }else if (argc == 2 && (argv[1] != "--h" && argv[1] != "help")){
         return 01;
     }
-    start_game();
+    Game *game = start_game();
+    play_game(game);
     return 0;
 }
 
+void help(){
+    
+}
 
+//#########################PLAY###############################
+
+void play_game(Game *game){
+    struct timespec delay;
+    delay.tv_sec = 0;
+    delay.tv_nsec = 200000000; // 500,000,000 nanoseconds = 0.5 seconds
+    nanosleep(&delay, NULL);
+    while (!check_win(game->grid)){
+        navigate(game->grid);
+    }
+    system("clear");
+    char *win_msg = (char*)malloc(sizeof(char) * 100), j = 0;
+    sprintf(win_msg, "Congratulations %s you just beat a level %c Sudoku!!!\n", game->player_name, game->lvl);
+    
+    for (size_t i = 0; i < 0x10; i++){
+        while (win_msg[j] != 00){
+            fputc(win_msg[j], stdout);
+            nanosleep(&delay, NULL);
+        }system("clear");
+    }
+    free(win_msg);
+}
+
+void navigate(char **grid){
+    //introduce the concept of readonly ...
+}
+
+
+char check_win(char **grid){
+    if (!check_rows(grid) || !check_cols(grid) || !check_rows(grid)){
+        return 00;
+    }
+    return 01;
+}
+
+char check_rows(char **grid){
+    char *found, val;
+    for (size_t i = 00; i < RANGE; i++){ // iterate over rows
+        found = (char*)calloc(sizeof(char), RANGE);
+        for (size_t j = 00; j < RANGE; j++){ // iterate over the row
+            val = (grid[i][j] ^ _MASK) -1; // convert char to int and sub1 for index alignement
+            if (found[val] == 01){
+                free(found);
+                return 00;
+            }
+            found[val] = 01;
+        }
+        free(found);
+    }
+    return 01;
+}
+
+char check_cols(char **grid){
+    char *found, val;
+    for (size_t i = 00; i < RANGE; i++){ // iterate over cols
+        found = (char*)calloc(sizeof(char), RANGE);
+        for (size_t j = 00; j < RANGE; j++){ // iterate over the col
+            val = (grid[j][i] ^ _MASK) -1; // convert char to int and sub1 for index alignement
+            if (found[val] == 01){
+                free(found);
+                return 00;
+            }
+            found[val] = 01;
+        }
+        free(found);
+    }
+    return 01;
+}
+
+char check_subgrids(char **grid){
+    
+}
+
+
+void display_grid(char **grid){
+    for (size_t i = 0; i < RANGE; i++){
+        printf("\t\t#####################################\n");
+        printf("\t\t");
+        for (size_t j = 0; j < RANGE; j++){
+            printf("# %c", grid[i][j]);
+        }printf(" #\n");
+    }
+}
+
+
+
+//#########################SETUP###############################
+// initialize a game
 Game *start_game(){
     Game *game = (Game*)malloc(sizeof(Game));
     game->player_name = (char*)malloc(sizeof(char) * LEN_NAME);
@@ -26,16 +118,18 @@ Game *start_game(){
         game->lvl = fgetc(stdin);
     }
     fill_grid(game);
+    return game;
 }
 
-
+// allocate memory for grid
 char **initialize_Grid(){
-    char **grid = (char**)malloc(sizeof(char*) * 9); // initialize rows
-    for (size_t i = 0; i < 9; i++)
-        *(grid+i) = (char*)malloc(sizeof(char) * 9); // initialize cols
+    char **grid = (char**)malloc(sizeof(char*) * RANGE); // initialize rows
+    for (size_t i = 00; i < RANGE; i++)
+        *(grid+i) = (char*)malloc(sizeof(char) * RANGE); // initialize cols
     return grid;
 }
 
+// fill memory with random values according to difficulty
 void fill_grid(Game *game){
     unsigned int seed = (unsigned int)time(NULL);
     srand(seed);
@@ -48,32 +142,305 @@ void fill_grid(Game *game){
     }
 }
 
+// (40) 4 of each + 4 others
 void fill_lvl1(char **grid){
-    Node nodes[9] = {
-        [0].val = ONE, [0].val = 0x0,
-        [1].val = TWO, [1].val = 0x0,
-        [2].val = THREE, [2].val = 0x0,
-        [3].val = FOUR, [3].val = 0x0,
-        [4].val = FIVE, [4].val = 0x0,
-        [5].val = SIX, [5].val = 0x0,
-        [6].val = SEVEN, [6].val = 0x0,
-        [7].val = EIGHT, [7].val = 0x0,
-        [8].val = NINE, [8].val = 0x0,
-    };
+    char val, row, col, i;
+    char others[REST_REP_L1] = {ONE, TWO, THREE, FOUR};
+    while (nodes[0].rep != MAX_REP_L1 || nodes[1].rep != MAX_REP_L1
+        || nodes[2].rep != MAX_REP_L1 || nodes[3].rep != MAX_REP_L1
+        || nodes[4].rep != MAX_REP_L1 || nodes[5].rep != MAX_REP_L1
+        || nodes[6].rep != MAX_REP_L1 || nodes[7].rep != MAX_REP_L1
+        || nodes[8].rep != MAX_REP_L1){
+        
+        val = (char)(rand() % RANGE + MIN_VAL);
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        if (nodes[(val-1) ^ _MASK].rep != MAX_REP_L1 && can_insert(grid, val, row, col)){
+            grid[row][col] = val;
+            nodes[val ^ _MASK].rep++;
+        }
+    }
+    // inserting eccess values
+    for (i = 0; i < REST_REP_L1; i++){
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        while (!can_insert(grid, others[i], row, col)){
+            row = (char)(rand() % RANGE + MIN_INDEX); 
+            col = (char)(rand() % RANGE + MIN_INDEX);
+        }
+        grid[row][col];
+        nodes[(others[i]-1) ^ _MASK].rep++;
+    }
 }
 
+// (35) 3 of each + 8 others
 void fill_lvl2(char **grid){
-
+    char val, row, col, i;
+    char others[REST_REP_L2] = {ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT};
+    while (nodes[00].rep != MAX_REP_L2 || nodes[01].rep != MAX_REP_L2
+        || nodes[02].rep != MAX_REP_L2 || nodes[03].rep != MAX_REP_L2
+        || nodes[04].rep != MAX_REP_L2 || nodes[05].rep != MAX_REP_L2
+        || nodes[06].rep != MAX_REP_L2 || nodes[07].rep != MAX_REP_L2
+        || nodes[010].rep != MAX_REP_L2){
+        
+        val = (char)(rand() % RANGE + MIN_VAL);
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        if (nodes[(val-01) ^ _MASK].rep != MAX_REP_L2 && can_insert(grid, val, row, col)){
+            grid[row][col] = val;
+            nodes[val ^ _MASK].rep++;
+        }
+    }
+    for (i = 00; i < REST_REP_L2; i++){
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        while (!can_insert(grid, others[i], row, col)){
+            row = (char)(rand() % RANGE + MIN_INDEX); 
+            col = (char)(rand() % RANGE + MIN_INDEX);
+        }
+        grid[row][col];
+        nodes[(others[i]-1) ^ _MASK].rep++;
+    }
 }
 
+// (30) 3 of each + 3 others
 void fill_lvl3(char **grid){
-
+    char val, row, col, i;
+    char others[REST_REP_L3] = {ONE, TWO, THREE};
+    while (nodes[00].rep != MAX_REP_L3 || nodes[01].rep != MAX_REP_L3
+        || nodes[02].rep != MAX_REP_L3 || nodes[03].rep != MAX_REP_L3
+        || nodes[04].rep != MAX_REP_L3 || nodes[05].rep != MAX_REP_L3
+        || nodes[06].rep != MAX_REP_L3 || nodes[07].rep != MAX_REP_L3
+        || nodes[010].rep != MAX_REP_L3){
+        
+        val = (char)(rand() % RANGE + MIN_VAL);
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        if (nodes[(val-01) ^ _MASK].rep != MAX_REP_L3 && can_insert(grid, val, row, col)){
+            grid[row][col] = val;
+            nodes[val ^ _MASK].rep++;
+        }
+    }
+    for (i = 00; i < REST_REP_L3; i++){
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        while (!can_insert(grid, others[i], row, col)){
+            row = (char)(rand() % RANGE + MIN_INDEX); 
+            col = (char)(rand() % RANGE + MIN_INDEX);
+        }
+        grid[row][col];
+        nodes[(others[i]-1) ^ _MASK].rep++;
+    }
 }
 
+// (25) 2 of each + 7 others
 void fill_lvl4(char **grid){
-
+    char val, row, col, i;
+    char rest[REST_REP_L5] = {ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT};
+    while (nodes[00].rep != MAX_REP_L4 || nodes[01].rep != MAX_REP_L4
+        || nodes[02].rep != MAX_REP_L4 || nodes[03].rep != MAX_REP_L4
+        || nodes[04].rep != MAX_REP_L4 || nodes[05].rep != MAX_REP_L4
+        || nodes[06].rep != MAX_REP_L4 || nodes[07].rep != MAX_REP_L4
+        || nodes[010].rep != MAX_REP_L4){
+        
+        val = (char)(rand() % RANGE + MIN_VAL);
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        if (nodes[(val-01) ^ _MASK].rep != MAX_REP_L4 && can_insert(grid, val, row, col)){
+            grid[row][col] = val;
+            nodes[val ^ _MASK].rep++;
+        }
+    }
+    for (i = 00; i < REST_REP_L4; i++){
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        while (!can_insert(grid, rest[i], row, col)){
+            row = (char)(rand() % RANGE + MIN_INDEX); 
+            col = (char)(rand() % RANGE + MIN_INDEX);
+        }
+        grid[row][col];
+        nodes[(rest[i]-01) ^ _MASK].rep++;
+    }
 }
 
+// (20) 2 of each + 2 others
 void fill_lvl5(char **grid){
-
+    char val, row, col, i;
+    char others[REST_REP_L5] = {ONE, TWO};
+    while (nodes[00].rep != MAX_REP_L5 || nodes[01].rep != MAX_REP_L5
+        || nodes[02].rep != MAX_REP_L5 || nodes[03].rep != MAX_REP_L5
+        || nodes[04].rep != MAX_REP_L5 || nodes[05].rep != MAX_REP_L5
+        || nodes[06].rep != MAX_REP_L5 || nodes[07].rep != MAX_REP_L5
+        || nodes[010].rep != MAX_REP_L5){
+        
+        val = (char)(rand() % RANGE + MIN_VAL);
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        if (nodes[(val-01) ^ _MASK].rep != MAX_REP_L5 && can_insert(grid, val, row, col)){
+            grid[row][col] = val;
+            nodes[val ^ _MASK].rep++;
+        }
+    }
+    for (i = 00; i < REST_REP_L5; i++){
+        row = (char)(rand() % RANGE + MIN_INDEX); 
+        col = (char)(rand() % RANGE + MIN_INDEX);
+        while (!can_insert(grid, others[i], row, col)){
+            row = (char)(rand() % RANGE + MIN_INDEX); 
+            col = (char)(rand() % RANGE + MIN_INDEX);
+        }
+        grid[row][col];
+        nodes[(others[i]-01) ^ _MASK].rep++;
+    }
 }
+
+char can_insert(char **grid, char val, char row, char col){
+    if (check_row(grid, val, row) && check_col(grid, val, col) && check_subgrid(grid, val, row, col)){
+        return 01;
+    }
+    return 00;
+}
+
+char check_insert_row(char **grid, char val, char row){
+    for (size_t i = 00; i < RANGE; i++){
+        if (grid[row][i] == val)
+            return 00;
+    }
+    return 01;
+}
+
+char check_insert_col(char **grid, char val, char col){
+    for (size_t i = 00; i < RANGE; i++){
+        if (grid[i][col] == val)
+            return 00;
+    }
+    return 01;
+}
+
+char check_insert_subgrid(char **grid, char val, char row, char col){
+    if (row < 03){ 
+        if (col < 03){
+            return check_insert_subgrid1(grid, val);
+        }else if (col > 02 && col < 06){
+            return check_insert_subgrid2(grid, val);
+        }else{
+            return check_insert_subgrid3(grid, val);
+        }
+    
+    }else if (row > 02 && row < 06){
+        if (col < 03){
+            return check_insert_subgrid4(grid, val);
+        }else if (col > 02 && col < 06){
+            return check_insert_subgrid5(grid, val);
+        }else{
+            return check_insert_subgrid6(grid, val);
+        }
+    
+    }else{
+        if (col < 03){
+            return check_insert_subgrid7(grid, val);
+        }else if (col > 02 && col < 06){
+            return check_insert_subgrid8(grid, val);
+        }else{
+            return check_insert_subgrid9(grid, val);
+        }
+    }
+}
+
+char check_insert_subgrid1(char **grid, char val){
+    char i, j;
+    for (i = 00; i < 03; i++){
+        for (j = 00; j < 03; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
+char check_insert_subgrid2(char **grid, char val){
+    char i, j;
+    for (i = 00; i < 03; i++){
+        for (j = 03; j < 06; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
+char check_insert_subgrid3(char **grid, char val){
+    char i, j;
+    for (i = 00; i < 03; i++){
+        for (j = 06; j < RANGE; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
+char check_insert_subgrid4(char **grid, char val){
+    char i, j;
+    for (i = 03; i < 06; i++){
+        for (j = 00; j < 03; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
+char check_insert_subgrid5(char **grid, char val){
+    char i, j;
+    for (i = 03; i < 06; i++){
+        for (j = 03; j < 06; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+}
+
+char check_insert_subgrid6(char **grid, char val){
+    char i, j;
+    for (i = 03; i < 06; i++){
+        for (j = 06; j < RANGE; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
+char check_insert_subgrid7(char **grid, char val){
+    char i, j;
+    for (i = 06; i < RANGE; i++){
+        for (j = 00; j < 03; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
+char check_insert_subgrid8(char **grid, char val){
+    char i, j;
+    for (i = 06; i < RANGE; i++){
+        for (j = 03; j < 06; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
+char check_insert_subgrid9(char **grid, char val){
+    char i, j;
+    for (i = 06; i < RANGE; i++){
+        for (j = 06; j < RANGE; j++){
+            if (grid[i][j] == val)
+                return 00;
+        }
+    }
+    return 01;
+}
+
--- games/sudoku.h
+++ games/sudoku.h
@@ -1,3 +1,8 @@
+
+
+#if !defined(SUDOKU_H)
+#define SUDOKU_H 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,12 +12,14 @@ typedef struct Game{
     char lvl;
     char **grid;
 }Game;
+
 typedef struct Node{
     char val;
     char rep;
 }Node;
 
 #define LEN_NAME    0x1E
+
 #define ONE         0X31
 #define TWO         0X32
 #define THREE       0X33
@@ -22,27 +29,78 @@ typedef struct Node{
 #define SEVEN       0X37
 #define EIGHT       0X38
 #define NINE        0X39
-#define RANGE       0xA
+
+#define MAX_REP_L1  0X4
+#define MAX_REP_L2  0X3
+#define MAX_REP_L3  0X3
+#define MAX_REP_L4  0X2
+#define MAX_REP_L5  0X2
+
+#define REST_REP_L1  04
+#define REST_REP_L2  010
+#define REST_REP_L3  03
+#define REST_REP_L4  07
+#define REST_REP_L5  02
+
+
+#define RANGE       0x9
+#define MIN_VAL     0x31
+#define MIN_INDEX   0x0
+#define _MASK       0b110000
+
 
 Node nodes[9] = {
-        [0].val = ONE, [0].val = 0x0,
-        [1].val = TWO, [1].val = 0x0,
-        [2].val = THREE, [2].val = 0x0,
-        [3].val = FOUR, [3].val = 0x0,
-        [4].val = FIVE, [4].val = 0x0,
-        [5].val = SIX, [5].val = 0x0,
-        [6].val = SEVEN, [6].val = 0x0,
-        [7].val = EIGHT, [7].val = 0x0,
-        [8].val = NINE, [8].val = 0x0,
+        [0].val = ONE, [0].rep = 0x0,
+        [1].val = TWO, [1].rep = 0x0,
+        [2].val = THREE, [2].rep = 0x0,
+        [3].val = FOUR, [3].rep = 0x0,
+        [4].val = FIVE, [4].rep = 0x0,
+        [5].val = SIX, [5].rep = 0x0,
+        [6].val = SEVEN, [6].rep = 0x0,
+        [7].val = EIGHT, [7].rep = 0x0,
+        [8].val = NINE, [8].rep = 0x0,
     };
 
-int main();
+int main(); // stack lvl1
 void help();
-Game *start_game();
-char **initialize_Grid();
-void fill_grid(Game *game);
-void fill_lvl1(char **grid);
-void fill_lvl2(char **grid);
-void fill_lvl3(char **grid);
-void fill_lvl4(char **grid);
-void fill_lvl5(char **grid);
+
+//#########################SETUP###############################
+Game *start_game(); // stack lvl2
+
+char **initialize_Grid(); // stack lvl3
+void fill_grid(Game *game); // stack lvl3
+
+void fill_lvl1(char **grid); // stack lvl4
+void fill_lvl2(char **grid); // stack lvl4
+void fill_lvl3(char **grid); // stack lvl4
+void fill_lvl4(char **grid); // stack lvl4
+void fill_lvl5(char **grid); // stack lvl4
+
+char can_insert(char **grid, char val, char row, char col); // stack lvl5
+char check_insert_row(char **grid, char val, char row); // stack lvl6
+char check_insert_col(char **grid, char val, char col); // stack lvl6
+char check_insert_subgrid(char **grid, char val, char row, char col); // stack lvl6
+
+char check_insert_subgrid1(char **grid, char val); // stack lvl7
+char check_insert_subgrid2(char **grid, char val); // stack lvl7
+char check_insert_subgrid3(char **grid, char val); // stack lvl7
+char check_insert_subgrid4(char **grid, char val); // stack lvl7
+char check_insert_subgrid5(char **grid, char val); // stack lvl7
+char check_insert_subgrid6(char **grid, char val); // stack lvl7
+char check_insert_subgrid7(char **grid, char val); // stack lvl7
+char check_insert_subgrid8(char **grid, char val); // stack lvl7
+char check_insert_subgrid9(char **grid, char val); // stack lvl7
+//########################################################
+//#########################play###############################
+
+void play_game(Game *game); // stack lvl2
+void navigate(char **grid); // stack lvl3
+void display_grid(char **grid); // stack lvl4
+
+char check_win(char **grid); // stack lvl3
+char check_rows(char **grid); // stack lvl4
+char check_cols(char **grid); // stack lvl4
+char check_subgrids(char **grid); // stack lvl4
+
+
+#endif // SUDOKU_H
